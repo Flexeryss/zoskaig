e26dc3326f1d16b37bf1c49f6bc9ab0e6d8d7085
Flexeryss <nikolas.hozza@gmail.com>
2024-12-09 09:31:54 +0100

    watchdog funguje

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,20 +1,21 @@
-
-
-// src/app/page.tsx
-
+// src/app/(home)/page.tsx
+ 
+ 
+ 
 import { getServerSession } from "next-auth";
-import { authOptions } from "../api/auth/[...nextauth]/authOptions";
-import AuthHomeView from "../../sections/AuthHomeView";
+import { authOptions } from "@/app/api/auth/[...nextauth]/authOptions";
 import NonAuthHomeView from "../../sections/NonAuthHomeView";
-
+import {redirect} from "next/navigation"
+ 
 export const metadata = { title: "Domov | ZoškaSnap" };
-
+ 
+ 
 export default async function HomePage() {
-  // Fetch session on the server
+ 
   const session = await getServerSession(authOptions);
-
-  // Conditionally render authenticated or non-authenticated home view
-  return session ? <AuthHomeView session={session} /> : <NonAuthHomeView />;
+  session ? redirect("/prispevok") : null;
+ 
+  return <NonAuthHomeView/>
 }
 
 
